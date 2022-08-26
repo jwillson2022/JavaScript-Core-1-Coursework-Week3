d485eb7a8819f9ba59726349fc7eaa87eda32c2a
jonathan willson <jwillson2022@gmail.com>
2022-08-26 17:29:33 +0100

    done d
    works no need for an array

--- 1-exercises/D-do-while/exercise.js
+++ 1-exercises/D-do-while/exercise.js
@@ -4,12 +4,21 @@
         - But in a do-while loop, because the condition is checked after the body, we know that it will always execute at least once
     
     Using a do-while loop, write a function which returns the sum of the first n even numbers (starting from 0)
-*/
+*/    
+let i = 0;
+sum =0;
 
 function evenNumbersSum(n) {
     // TODO
+do {
+let answer = i * 2 ;    // 0,2,4
+i++;
+//array.push(answer);
+sum += answer;
+} while (n > i);   //returns the sum of the first n even numbers (starting from 0)
+return sum;
 }
 
-console.log(evenNumbersSum(3)); // should output 6
-console.log(evenNumbersSum(0)); // should output 0
-console.log(evenNumbersSum(10)); // should output 90
+//console.log(evenNumbersSum(3)); // should output 6 //  0+2+4=6
+//console.log(evenNumbersSum(0)); // should output 0
+//console.log(evenNumbersSum(10)); // should output 90 /2+4+6+8=10=12=14=16=18
